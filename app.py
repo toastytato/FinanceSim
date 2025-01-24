00acce39aaf765f58d64cd80c0e6d60d2414a620
[Shawn Hsueh] <[shawionghsueh@utexas.edu]>
2025-01-24 16:18:03 -0500

    changed back to side column plot view, added ability to modify config

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 from premade_actions import *
 from sim_framework import *
 
-st.set_page_config(page_title="FinSim", layout="centered")
+st.set_page_config(page_title="FinSim", layout="wide")
 
 USE_DEBUG = st.secrets.get("USE_DEBUG", False)
 IS_LOCAL = st.secrets.get("IS_LOCAL", False)
@@ -74,21 +74,21 @@ example_json = """
 
 example_json = """
 {
-"scenarios": {
-    "HousePurchaseWithRefinance": {
-        "account_names": {
-            "myself": 0,
-            "mortgage": 0,
-            "house": 0,
-            "family_debt": 0,
+"scenarios": {  // list of scenarios to compare. 
+    "HousePurchaseWithRefinance": { // relevant name for the scenario at hand
+        "account_names": { // only track accounts relevant to net worth of the user (cash, assets and liabilities)
+            "myself": 0, // cash of the user (initial value)
+            "mortgage": 0, // liability of the user (initial value)
+            "house": 0, // asset of the user (initial value)
+            "family_debt": 0, // liability of the user (initial value)
         },
-        "variables": {
+        "variables": { // only created if there will be an action changing the value during the sim
             "loan_rate_apr": 0.075,
             "fam_rate_apr": 0.04,
         },
         "actions": [
             {
-                "function": "add2sim_buy_house",
+                "function": "add2sim_buy_house", // uses the available add2sim functions
                 "kwargs": {
                     "mortgage_account_name": "mortgage",
                     "lender_account_name": "bank",
@@ -108,7 +108,7 @@ example_json = """
             {
                 "function": "add2sim_modify_variable", // refinances the loan
                 "kwargs": {
-                    "variable_name": "loan_rate_apr",
+                    "variable_name": "loan_rate_apr", // references a variable from the "variables" field
                     "new_value": 0.06,
                     "modification_date": "01/01/2026",
                 },
@@ -124,7 +124,7 @@ example_json = """
                 },
             },
             {
-                "function": "add2sim_sell_house",
+                "function": "add2sim_sell_house", 
                 "kwargs": {
                     "seller_account_name": "myself",
                     "house_val_account_name": "house",
@@ -135,11 +135,11 @@ example_json = """
             },
         ],
     },
-    "Renting": {
-        "account_names": {
+    "Renting": { // note how a new scenario was screated for this separate comparison case
+        "account_names": { // only tracks "myself" since no asset or liabilities to manage
             "myself": 0,
         },
-        "variables": {},
+        "variables": {}, // no variables to track in this scenario because nothing changes in midway in the sim
         "actions": [
             {
                 "function": "add2sim_recurring_transaction",
@@ -160,38 +160,43 @@ example_json = """
 """
 
 system_prompt = f"""
-1. You are a financial adviser that uses the available tools to create simulation scenarios that matches the user's requests
-2. Use the following add2sim functions and match its parameters as kwargs (IGNORE THE "sim:" PARAMETER IN THE FUNCTION DOCS):
-{get_function_docs()}
-3. This is an example JSON schema to follow:
-```
-{example_json}
-```
-4. Each scenario must be completely self-contained with ALL necessary actions
-- Never rely on actions from other scenarios
-- Any kwargs with a suffix of "_varname" MUST be a variable name that is defined in the variables section
-- A start and end date MUST be provided for each scenario
-- You are outputting in Markdown (but don't respond starting in ```), so all dollar signs be written like \$
-- Use PascalCase for the variable names, but snake_case for the function calls
-5. After creating the JSON, summarize each of the scenarios you created
-6. **The account names should just track values associated with the current user's net worth (the user, their assets and debts/liabilities), nobody else**.
-7. Variables should only be created if the user asks to modify some value halfway into the sim. Otherwise the add2sim premade functions will handle hardcoded values as indicated in the docs
-8. If the function parameter allows for either str or float, the str value MUST BE INSTANTIATED IN THE "variables" DICTIONARY
-9. If the variable is not being modified, no need to make it a variable. Just put a hardcoded value in the parameter instead
-10. Do not shorten the final JSON. Every response should give the full JSON needed to simulate the user's request
-11. You are just to help interface the user with the simulation engine, everything else is handled for you.
-12. Do not hallucinate kwargs for the add2sim functions
-13. If the user request cannot be accomplished with the given functions, let them know and do not generate the scenarios
-14. If you are making a comparison, each one will need its own scenario
+You are a financial adviser with access to simulation tools to fulfill the user's requests
+Use the following add2sim functions and match its parameters as kwargs (IGNORE THE "sim:" PARAMETER IN THE FUNCTION DOCS): {get_function_docs()}
+Do not hallcuinate or make up functions nor kwargs
+This is an example JSON schema to follow: ```{example_json}```
+You will respond to the user to answer their questions, and if the context makes sense create a JSON code block to create the simulation setup
+Assume the system you're in will replace the JSON with the simulation output, so do not refer to the JSON. Simply refer to it as the simulation output
+You are outputting in markdown, so all dollar signs should be written like \$
+The account names must only track accounts relevant to the user's net worth
+When you make a change, do NOT refer back to the specific configuration implementation you have generated
+Every scenario in the list of scenarios must be fully self contained. Do not combine different scenarios into one
 """
+
+# other = """
+# 4. Each scenario must be completely self-contained with ALL necessary actions
+# - Never rely on actions from other scenarios
+# - Any kwargs with a suffix of "_varname" MUST be a variable name that is defined in the variables section
+# - A start and end date MUST be provided for each scenario
+# - You are outputting in Markdown (but don't respond starting in ```), so all dollar signs be written like \$
+# - Use PascalCase for the variable names, but snake_case for the function calls
+# 5. After creating the JSON, summarize each of the scenarios you created
+# 6. **The account names should just track values associated with the current user's net worth (the user, their assets and debts/liabilities), nobody else**.
+# 7. Variables should only be created if the user asks to modify some value halfway into the sim. Otherwise the add2sim premade functions will handle hardcoded values as indicated in the docs
+# 8. If the function parameter allows for either str or float, the str value MUST BE INSTANTIATED IN THE "variables" DICTIONARY
+# 9. If the variable is not being modified, no need to make it a variable. Just put a hardcoded value in the parameter instead
+# 10. Do not shorten the final JSON. Every response should give the full JSON needed to simulate the user's request
+# 11. You are just to help interface the user with the simulation engine, everything else is handled for you.
+# 12. Do not hallucinate kwargs for the add2sim functions
+# 13. If the user request cannot be accomplished with the given functions, let them know and do not generate the scenarios
+# 14. If you are making a comparison, each one will need its own scenario
+# """
 
 initiate_welcome_prompt = """
 Now that you have the instructions, a user has just joined. 
 You are a helpful financial adviser who will answer their questions and setup the relevant scenarios when appropriate.
 1. Welcome them in and explain what the user can do (give a variety of example prompts, some which are comparisons). 
-2. Select one of the prompts you gave and then create the simulation setup in JSON
-    - assume the json will be replaced by a plot, so do NOT say here's the json
-3. List out the values you chose and tie it back to the context of the request. Assume the user does not see the json you just generated and keep it concise
+2. Select one of the prompts you gave and then create the simulation setup following the schema shown previously. Say you are generating the simulation setup
+3. Summarize the values you chose and tie it back to the context of the request. Use bullet points
 4. Ask the user to try modifying the config in general terms, make up a scenario for them, or just ask you general finance questions.
 Keep this short"""
 
@@ -217,32 +222,6 @@ Just chat with it to get personalized advice!
 # Perfect for anyone looking to visualize and optimize their financial future."""
 
 
-# def parse_json(text: str) -> tuple[bool, dict]:
-#     """Extract content between triple backticks and parse as JSON.
-#     Returns (success, parsed_json)"""
-#     try:
-#         # Split by triple backticks and get the content
-#         parts = text.split("```")
-#         if len(parts) < 3:
-#             return False, {}
-
-#         # Get the content and clean it
-#         json_text = parts[1].replace("json", "").replace(" ", "")
-#         # Remove comments that start with //
-#         json_text = "\n".join(line.split("//")[0] for line in json_text.split("\n"))
-
-#         # Parse JSON and return it directly
-#         return True, json.loads(json_text)
-
-#     except Exception as e:
-#         print(e)
-#         return False, None
-
-
-# def trim_code_blocks(text: str) -> str:
-#     return "\n".join(part for i, part in enumerate(text.split("```")) if i % 2 == 0)
-
-
 def parse_response(response):
     precode_text = ""
     code_text = ""
@@ -257,6 +236,9 @@ def parse_response(response):
         code_text = split[1].split("```")[0]
         postcode_text = split[1].split("```")[1]
 
+    # Remove comments that start with //
+    code_text = "\n".join([line.split("//")[0] for line in code_text.split("\n")])
+
     return precode_text, code_text, postcode_text
 
 
@@ -270,7 +252,7 @@ def main():
     if "model" not in st.session_state:
         st.session_state.model = None
 
-    user_input = st.chat_input("Let's chat!")
+    chat_input = st.chat_input("Let's chat!")
     is_system = False
 
     # Remove the two-column layout and just use a single column
@@ -298,7 +280,7 @@ def main():
                 google_api_key=google_api_key,
                 temperature=0,
             )
-            user_input = system_prompt + "\n" + initiate_welcome_prompt
+            chat_input = system_prompt + "\n" + initiate_welcome_prompt
             is_system = True
 
             try:
@@ -310,52 +292,59 @@ def main():
             st.success("API Key updated successfully!")
 
     # Replace the left_col with a single container
-    chat_container = st.container(border=True)
+    left_col, right_col = st.columns(2, vertical_alignment="bottom")
+    with left_col:
+        chat_container = st.container(border=True)
+    with right_col:
+        test_container = st.container(border=True, height=730)
 
     with chat_container:
         st.title("Chat")
         # Display chat history
-        display_chat_history(user_input)
+        display_chat_history(chat_input)
 
         # only executed when there is a new entry from the user
         # displays the response in realtime from the LLM API
-        if user_input and st.session_state.model:
+        if chat_input and st.session_state.model:
             # don't show system prompt when in production
             if not is_system or USE_DEBUG:
                 with st.chat_message("user"):
-                    st.markdown(user_input)
-            st.session_state.messages.append({"role": "user", "content": user_input})
+                    st.text(chat_input)
+            st.session_state.messages.append({"role": "user", "content": chat_input})
 
             # Invoke the model with the system message and user input
             llm_streamer = st.session_state.model.stream(st.session_state.messages)
 
-            sims = []
             with st.chat_message("assistant"):
                 full_response = display_llm_stream(llm_streamer)
+                st.session_state.messages.append(
+                    {"role": "assistant", "content": full_response}
+                )
                 # Add responses to chat history
                 pre, code, post = parse_response(full_response)
                 # the response contains setup code for the simulation
                 # pre is displayed during the display_llm_stream
 
-                try:
-                    data = json.loads(code)
-                except Exception as e:
-                    st.text(code)
-                    data = {}
-
                 if code:
+                    try:
+                        data = json.loads(code)
+                    except Exception as e:
+                        # bad code was generated
+                        st.error(e)
+                        st.code(code)
+                        data = None
+                else:
+                    # just a normal response without code
+                    data = None
 
+                if data:
                     with st.spinner("Running sims..."):
                         st.session_state.sim_config = data
-                        st.session_state.sims = Sim.get_sims_from_config(
-                            st.session_state.sim_config
-                        )
-                        sim = st.session_state.sims
-                        for sim in st.session_state.sims:
-                            sim.run()
 
                     with st.spinner("Plotting results..."):
-                        plot_sim_data()
+                        # plot_sim_data(1)
+                        with st.popover("See the Generated Config"):
+                            st.json(st.session_state.sim_config)
 
                 if post:
                     post_code_text = ""
@@ -365,34 +354,61 @@ def main():
                         post_container.markdown(post_code_text)
                         time.sleep(0.01)
 
-                st.session_state.messages.append(
-                    {"role": "assistant", "content": full_response}
-                )
+    with test_container:
+        config_tab, plot_tab = st.tabs(["Configs", "Plot"])
+        with config_tab:
+            config_input = st.text_area(
+                "Configs",
+                value=json.dumps(st.session_state.sim_config, indent=2),
+                height=550,
+            )
+            st.session_state.sim_config = json.loads(config_input)
+
+            rerun = st.button("Update Plot")
+            if rerun:
+                with st.spinner("Running sims..."):
+                    run_sims()
+        with plot_tab:
+            plot_sim_data(3)
 
 
-def display_chat_history(user_input):
+def run_sims():
+    st.session_state.sims = Sim.get_sims_from_config(st.session_state.sim_config)
+    sim = st.session_state.sims
+    for sim in st.session_state.sims:
+        sim.run()
+
+
+def display_chat_history(chat_input):
     start_msg_idx = 0 if USE_DEBUG else 1
-    for message in st.session_state.messages[start_msg_idx:]:
+    if USE_DEBUG:
+        all_msgs = st.session_state.messages
+    else:
+        all_msgs = st.session_state.messages[1:][-6:]
+    for message in all_msgs:
         is_latest = message == st.session_state.messages[-1]
         with st.chat_message(message["role"]):
             if message["role"] == "user":
-                st.markdown(message["content"])
+                st.text(message["content"])
             elif message["role"] == "assistant":
                 # if we are updating that chat NOT from a user input
                 pre, code, post = parse_response(message["content"])
                 st.markdown(pre)
                 if code:
-                    if is_latest and not user_input:
-                        plot_sim_data()
-                        st.markdown(post)
+                    if is_latest and not chat_input:
+                        # occurs when the screen gets refreshsed
+                        # and we're redrawing last output NOT triggered by user input
+                        # plot_sim_data(2)
+                        pass
                     else:
                         with st.popover("See Generated Code"):
                             st.json(code)
-                        st.markdown(post)
+                    st.markdown(post)
 
 
 def display_llm_stream(llm_streamer):
     # parses what the LLM is saying in real-time
+    # stops streaming when code block is reached
 
     pre_code_response_container = st.empty()
 
@@ -413,9 +429,8 @@ def display_llm_stream(llm_streamer):
             if "```" in word:
                 # sometimes the LLM combines other words with the delimiter
                 in_code_block = True
-                code_delimiter_word = word.split("```")
                 if in_code_block:
-                    pre_code_response += code_delimiter_word[0]
+                    pre_code_response += word.split("```")[0]
                 break
             else:
                 pre_code_response += word + " "
@@ -437,7 +452,11 @@ def display_llm_stream(llm_streamer):
     return full_response_text
 
 
-def plot_sim_data():
+def plot_sim_data(key):
+    # Plot simulation data using Plotly
+    # The 'key' parameter is used to uniquely identify the plot in Streamlit.
+    # This ensures that the plot updates correctly when the data changes.
+
     if "sims" in st.session_state and st.session_state.sims:
         desired_sims = st.session_state.sims
 
@@ -448,6 +467,7 @@ def plot_sim_data():
             "Select accounts to aggregate:",
             options=list(all_columns),
             default=list(all_columns),
+            key=f"multisel{key}",
         )
 
         if selected_columns:
@@ -460,6 +480,7 @@ def plot_sim_data():
                     )
 
                     def calculate_amt_mod(row, selected_columns):
+                        # calculate modified amount based on who the money is going to
                         if (
                             row["from"] in selected_columns
                             and row["to"] in selected_columns
@@ -542,10 +563,7 @@ def plot_sim_data():
                         x=0.5,
                     ),
                 )
-                st.plotly_chart(fig, use_container_width=True)
-
-                with st.popover("See the Generated Config"):
-                    st.json(st.session_state.sim_config, expanded=10)
+                st.plotly_chart(fig, use_container_width=True, key=f"plotly{key}")
             except Exception as e:
                 st.error(f"Error displaying plot: {str(e)}")
 
